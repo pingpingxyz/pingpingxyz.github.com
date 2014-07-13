feb9a2a04fd9d4079cbea0ffb6f8867a05a4ccd6
unknown <regexp-lin@163.com>
2014-07-13 11:01:19 +0800

    add the clock

--- src/layout/body.js
+++ src/layout/body.js
@@ -14,11 +14,7 @@ define(function(require, exports, module) {
 			var tpl = [
 				'<div class="container">',
 				'<div class="list"></div>',
-				'<div class="side">',
-					'<object height="180" width="180" align="center" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0">',
-			            '<param value="style/clock.swf" name="movie">',
-			        '</object>',
-				'</div>'
+				'<div class="side"></div>'
 			].join('');
 
 			this.el = $(tpl).appendTo(ct);
--- src/layout/sideBar.js
+++ src/layout/sideBar.js
@@ -13,7 +13,11 @@ define(function(require, exports, module) {
 			var tpl = [
 				'<div class="side-container">', 
 					'<div class="calendar-wrap"></div>', 
-					'<div class="clock-wrap"></div>',
+					'<div class="clock-wrap">',
+						'<object height="180" width="180" align="center" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0">',
+				            '<object type="application/x-shockwave-flash" data="./style/clock.swf" style="width:250px; height:250px;opacity:0.5;">',
+				        '</object>',
+					'</div>',
 				'</div>'
 			].join(''); 
 
